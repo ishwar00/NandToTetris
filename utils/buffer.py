bd490edf90801c814af734f8b5cddc4356dec1a8
ishwar <ishwargowda100@gmail.com>
2022-03-24 10:11:40 +0530

    added project 02

--- utils/buffer.py
+++ utils/buffer.py
@@ -1,8 +1,8 @@
 
 file = open("utils\output.txt", 'w')
 
-for i in range(2, 16):
-    store = f"    Or(a = in[{i}], b = a{i - 1}, out = a{i});\n"
+for i in range(1, 16):
+    store = f"FullAdder(a = a[{i}], b = b[{i}], c = carry{i - 1}, carry = carry{i}, sum = out[{i}]);\n"
     file.writelines(store)
 
 file.close()
