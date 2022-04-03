c55c89c5dacdd5faacb91629a82c988ecffbb452
ishwar <ishwargowda100@gmail.com>
2022-04-03 12:14:13 +0530

    done with project 03

--- utils/buffer.py
+++ utils/buffer.py
@@ -1,8 +1,8 @@
 
-file = open("utils\output.txt", 'w')
+file = open("H:\\stay_away\\NandToTetris\\NandToTetris\\utils\\output.txt", 'w')
 
-for i in range(1, 16):
-    store = f"FullAdder(a = a[{i}], b = b[{i}], c = carry{i - 1}, carry = carry{i}, sum = out[{i}]);\n"
+for i in range(0, 8):
+    store = f'    RAM8(in = i, load = R{i}, address = address[2..0], out = out{i});\n'
     file.writelines(store)
 
 file.close()
