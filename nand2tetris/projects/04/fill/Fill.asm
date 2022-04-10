3474b5268f1deaa266829429f9bf91e1a11f4018
ishwar <ishwargowda100@gmail.com>
2022-04-10 09:10:27 +0530

    Done with project 04 :)

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -12,3 +12,62 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+    // size = 8192 memory block size
+    @8192
+    D=A
+    @size
+    M=D
+(INITIALISE-BLACKING)
+// i = 0 
+    @i
+    M=0
+(BLACKING)
+    // KBD != 0
+    @KBD
+    D=M
+    @INITIALISE-CLEANING
+    D;JEQ
+    // okay KBD != 0 true 
+    // i != size 
+    @size
+    D=M
+    @i
+    D=D-M
+    @BLACKING
+    D;JEQ
+    // i != size is true 
+    @SCREEN
+    D=A
+    @i
+    A=D+M
+    M=-1 // RAM[SCREEN + i] = -1 
+    @i
+    M=M+1
+    @BLACKING
+    0;JMP
+(INITIALISE-CLEANING)
+    // i = 0 
+    @i
+    M=0
+(CLEANING)
+    @KBD
+    D=M
+    @INITIALISE-BLACKING
+    D;JNE
+    // KBD == 0 is true 
+    @i
+    D=M
+    @size
+    D=D-M
+    @CLEANING
+    D;JEQ
+    // i != size is true 
+    @SCREEN
+    D=A
+    @i
+    A=D+M
+    M=0 // RAM[SCREEN + i] = 0 
+    @i
+    M=M+1
+    @CLEANING
+    0;JMP
--- nand2tetris/projects/04/mult/Mult.asm
+++ nand2tetris/projects/04/mult/Mult.asm
@@ -10,3 +10,25 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+    @i
+    M=0
+    @R2
+    M=0
+(LOOP)
+    @i
+    D=M
+    @R0
+    D=D-M
+    @END
+    D;JEQ
+    @R1
+    D=M
+    @R2
+    M=D+M
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
